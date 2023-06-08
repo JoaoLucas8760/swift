2bdb4a3848d11557854ecd56f69b77da7a3d89c4
joaolucas8760 <canalluka8760@gmail.com>
2023-06-08 12:20:21 -0400

    commit

---
+++ saltos magicos/saltos magicos/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  saltos magicos
+//
+//  Created by user241276 on 6/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello world, swift ")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ saltos magicos/saltos magicos/saltos_magicosApp.swift
@@ -0,0 +1,17 @@
+//
+//  saltos_magicosApp.swift
+//  saltos magicos
+//
+//  Created by user241276 on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct saltos_magicosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
